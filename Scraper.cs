e40b27a4515581e0fc08380a712fe074a9dbb867
Emre Ekinci <yunusemrem@windowslive.com>
2019-08-04 16:47:18 +0300

    fixed exported Excel, fixed  dateInterval issues

--- Scraper.cs
+++ Scraper.cs
@@ -14,15 +14,26 @@ using System.Drawing;
 using Microsoft.Office.Interop.Excel;
 using Linkedin_Scrapper.Properties;
 
-//           işlemlerden sonra print yap
+/*
+
+    işlemlerden sonra print yap, işlemi yaparken değil
+    yani Person clasına print yaz
+
+    proifle TITLES >>>
+
+    LOCATION    class="t-16 t-black t-normal inline-block"  
+    POSITION    class="mt1 t-18 t-black t-normal"	
+    NAME        inline t-24 t-black t-normal break-words	
+
+*/
 
 namespace Linkedin_Scrapper
 {
     class Scrapper
     {
         static public IWebDriver driver     = new ChromeDriver("../../");
-        static public string loginID        = "YOUR_LOGIN_CRED_MAIL";
-        static public string loginPassword  = "YOUR_LOGIN_CRED_PASS";
+        static public string loginID        = "YOUR CREDS";
+        static public string loginPassword  = "YOUR CREDS";
 
         [STAThread]
         static void Main()
@@ -34,6 +45,7 @@ namespace Linkedin_Scrapper
             Console.OutputEncoding = Encoding.UTF8;
             Console.WriteLineFormatted("\tCode Page is set to   : " + Console.OutputEncoding.WebName,Color.LightGoldenrodYellow);
 
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
             driver.Navigate().GoToUrl("https://www.linkedin.com/login?trk=guest_homepage-basic_nav-header-signin");
             driver.FindElement(By.XPath("//*[@id=\"username\"]")).SendKeys(loginID);
             driver.FindElement(By.XPath("//*[@id=\"password\"]")).SendKeys(loginPassword);
@@ -48,35 +60,41 @@ namespace Linkedin_Scrapper
             List<Person> personList = new List<Person> { };
             foreach (string personPage in personPages)
             { // process each user
-                //int clr = 1 + personPages.IndexOf(personPage)*7;
-                //Console.BackgroundColor =  Color.FromArgb(clr,clr,clr); 
-                Console.WriteLineFormatted("===============================================================================",Color.Red);
-                Scrapper.driver.Navigate().GoToUrl(personPage);
-
-                // clicking show more buttons
-                var showMoreButtons = Scrapper.driver.FindElements(By.ClassName("pv-profile-section__text-truncate-toggle")).ToList();
-                var expectedButtons = new List<string> { "more role", "more education", "more experience" };
-                var clickButtons    = showMoreButtons.Where(button => expectedButtons.Count(expcButton => button.Text.Contains(expcButton)) > 0).ToList();
-                foreach (var bt in clickButtons)
-                    bt.Click();
-
-                //wait to make sure buttons clicked and data is loaded properly
-                while (true)
+                try
                 {
-                    var showFewerButtons = Scrapper.driver.FindElements(By.ClassName("pv-profile-section__text-truncate-toggle")).ToList();
-                    var waitedButtons    = new List<string> { "fewer role", "fewer education", "fewer experience" };
-                    var fewButtons       = showFewerButtons.Where(button => waitedButtons.Count(wButtons => button.Text.Contains(wButtons)) > 0).ToList();
-                    if (fewButtons.Count < clickButtons.Count)
-                        Thread.Sleep(500);
-                    else
-                        break;
+                    Console.WriteLineFormatted("===============================================================================================", Color.Red);
+                    Scrapper.driver.Navigate().GoToUrl(personPage);
+
+                    // clicking show more buttons
+                    var showMoreButtons = Scrapper.driver.FindElements(By.ClassName("pv-profile-section__text-truncate-toggle")).ToList();
+                    var expectedButtons = new List<string> { "more role", "more education", "more experience" };
+                    var clickButtons = showMoreButtons.Where(button => expectedButtons.Count(expcButton => button.Text.Contains(expcButton)) > 0).ToList();
+                    foreach (var bt in clickButtons)
+                        bt.Click();
+
+                    //wait to make sure buttons clicked and data is loaded properly
+                    while (true)
+                    {
+                        var showFewerButtons = Scrapper.driver.FindElements(By.ClassName("pv-profile-section__text-truncate-toggle")).ToList();
+                        var waitedButtons = new List<string> { "fewer role", "fewer education", "fewer experience" };
+                        var fewButtons = showFewerButtons.Where(button => waitedButtons.Count(wButtons => button.Text.Contains(wButtons)) > 0).ToList();
+                        if (fewButtons.Count < clickButtons.Count)
+                            Thread.Sleep(500);
+                        else
+                            break;
+                    }
+                    // get user infos
+                    Person tempPerson = new Person(personPage);
+                    tempPerson.writeToExcel();
+                    personList.Add(tempPerson);
                 }
-                // get user infos
-                Person person = new Person(personPage);
-                person.writeToExcel();
-                personList.Add(person);
+                catch(Exception ee)
+                {
+                    Console.WriteLineFormatted("\n\t> Something wrong with person : " + personPage, Color.Red);
+                    Console.WriteLineFormatted("\t> Exception : " + ee.Message, Color.Orange);
+                }
             }
-            Console.WriteLineFormatted("============================    DONE    =====================================", Color.Red);
+            Console.WriteLineFormatted("============================    ALL DONE    =====================================", Color.Red);
             ReadLine();
             driver.Quit();
         }
@@ -87,9 +105,9 @@ namespace Linkedin_Scrapper
     /// </summary>
     public class Person
     {
-        static public List<Exp> experiences = new List<Exp> { };
-        static public List<Edu> educations  = new List<Edu> { };
-        static public List<string> languages= new List<string>();
+        static public List<Exp> experiences  = new List<Exp> { };
+        static public List<Edu> educations   = new List<Edu> { };
+        static public List<string> languages = new List<string>() {"Language1","Language2","Language3" };
         static public string fullName   = "Full Name";
         static public string currTitle  = "Current Title";
         static public string currPos    = "Current Position";
@@ -101,10 +119,19 @@ namespace Linkedin_Scrapper
             var eduInfos  = Scrapper.driver.FindElements(By.ClassName("pv-education-entity"));
             var expInfos  = Scrapper.driver.FindElements(By.ClassName("pv-position-entity"));
 
-            fullName = personPage.Split('/')[4];
-            Write("\r\n\t\t" + fullName + "\r\n");
+            List<string> currentInfos = Scrapper.driver.FindElement(By.CssSelector(".flex-1.mr5")).Text.Replace("\r","").Split('\n').ToList();
+            int trashIndex = currentInfos.FindIndex(inf => inf.Contains("degree connection") == true);
+            if(trashIndex > -1)
+                currentInfos.RemoveRange(trashIndex,2);
+            currentInfos = currentInfos.Where(inf => inf.Contains("has a account") == false).ToList();
 
-            Console.WriteFormatted(" |\r\n | EDUCATION:__________________________________________________________________", Color.Cyan);
+            fullName    = currentInfos[0];
+            currTitle   = currentInfos[1];
+            trashIndex  = currentInfos[2].Split(' ').ToList().IndexOf("connections");
+            currPos     = String.Join(",", currentInfos[2].Split(' ').ToList().GetRange(0, trashIndex - 1));
+
+            Write("\r\n\t\t" + fullName + "\r\n");
+            Console.WriteFormatted(" |\r\n | EDUCATION:__________________________________________________________________\n", Color.Cyan);
             foreach (var eduInfo in eduInfos)
             {
                 var attrs   = eduInfo.Text.Replace("\r", "").Split('\n').ToList();
@@ -113,7 +140,7 @@ namespace Linkedin_Scrapper
                 educations.Add(newEdu);
             }
 
-            Console.WriteFormatted(" |\r\n | EXPERIENCE:__________________________________________________________________", Color.Cyan);
+            Console.WriteFormatted(" |\r\n | EXPERIENCE:__________________________________________________________________\n", Color.Cyan);
             foreach (var expInfo in expInfos)
             {
                 var attrs   = expInfo.Text.Replace("\r", "").Split('\n').ToList();
@@ -125,13 +152,6 @@ namespace Linkedin_Scrapper
 
         public void writeToExcel() 
         {
-            /*
-             *  AYLARı duration dan sil
-             *  bir exp şirket için total duration değil zaman aralığı 
-             *  kalın yaz
-             *  borderlar sıkıntılı eski template ten kopyala
-             * 
-             * */
             string templateFile = "../../Resources/template.xlsx"; // @"YOUR_EXCEL_FILE_PATH";
             string userFile     = "./" + fullName + ".xlsx";
             File.Copy(templateFile, userFile);
@@ -149,14 +169,17 @@ namespace Linkedin_Scrapper
             foreach (Edu edu in educations) // write education column(4)
             {
                 worksheet.Cells[rowCounter * 2, 4].Value = edu.schoolName + ", " +  edu.date ;
+                worksheet.Cells[rowCounter * 2, 4].Font.Bold = true;
                 worksheet.Cells[rowCounter * 2 + 1, 4].Value = edu.field;
                 rowCounter++;
             }
             rowCounter = 2;
             foreach (Exp exp in experiences) // write experiences columns(2,3)
             {   
-                worksheet.Cells[rowCounter, 2].Value = exp.totalDuration ;
+                worksheet.Cells[rowCounter, 2].Value = exp.dateIterval;
+                worksheet.Cells[rowCounter, 2].Font.Bold = true;
                 worksheet.Cells[rowCounter, 3].Value = exp.companyName;
+                worksheet.Cells[rowCounter, 3].Font.Bold = true;
                 rowCounter++;
                 foreach (Job job in exp.jobs)
                 {   
@@ -167,11 +190,17 @@ namespace Linkedin_Scrapper
             }
             // write user info column(1)
             worksheet.Cells[2, 1].Value = fullName;
+            worksheet.Cells[2, 1].Font.Bold = true;
+
             worksheet.Cells[3, 1].Value = currTitle ;
             worksheet.Cells[4, 1].Value = currPos;
+
             worksheet.Cells[5, 1].Value = "Date Birth";
+            worksheet.Cells[5, 1].Font.Bold = true;
             worksheet.Cells[6, 1].Value = dateBrith;
+
             worksheet.Cells[7, 1].Value = "Languages";
+            worksheet.Cells[7, 1].Font.Bold = true;
             for (var i = 0; i < languages.Count; i++)
                 worksheet.Cells[8 + i,1].Value = languages[i];
 
@@ -188,7 +217,9 @@ namespace Linkedin_Scrapper
     {
         public string companyName;
         public string totalDuration;
+        public string dateIterval;
         public List<Job> jobs = new List<Job> { };
+        public List<string> months = new List<string>() { "Jan ", "Feb ", "Mar ", "Apr ", "May ", "Jun ", "Jul ", "Aug ", "Sep ", "Oct ", "Nov ", "Dec " };
 
         public Exp(List<string> exp)
         {
@@ -215,13 +246,20 @@ namespace Linkedin_Scrapper
                 if (indexOfSecondExp != -1)
                     exp.RemoveRange(exp.IndexOf("Title"), indexOfSecondExp);
             }
+
+            var tempStartDates = jobs.Select(jj => jj.dateIterval.Split('–')[0]).ToList();
+            var tempEndDates = jobs.Select(jj => jj.dateIterval.Split('–')[1]).ToList();
+            tempStartDates.Sort();
+            tempEndDates.Sort();
+            dateIterval = tempStartDates.First() + " to " + tempEndDates.Last();
+            months.Where(mm => dateIterval.Contains(mm)).ToList().ForEach(existmm => dateIterval = dateIterval.Replace(existmm,""));
         }
         internal string expPrint()
         {
-            var ret = "\r\n\t>>" + companyName + "\t" + totalDuration + "\r\n";
+            var ret = "\r\n\t>>" + companyName + "\t" + dateIterval;
+            Console.WriteLine(ret,Color.LightGoldenrodYellow);
             foreach (Job job in jobs)
-                ret += job.eprint();
-            Console.WriteLine(ret);
+                job.eprint();
             return ret;
         }
     }
@@ -233,12 +271,18 @@ namespace Linkedin_Scrapper
         public string title;
         public string dateIterval;
         public string empDuration;
+        public List<string> months = new List<string>() { "Jan ", "Feb ", "Mar ", "Apr ", "May ", "Jun ", "Jul ", "Aug ", "Sep ", "Oct ", "Nov ", "Dec " };
 
         public Job(List<string> jobInfos)
         {
             title       = jobInfos.IndexOf("Title") != -1 ? jobInfos[jobInfos.IndexOf("Title") + 1] : "??";
             empDuration = jobInfos.IndexOf("Employment Duration") != -1 ? jobInfos[jobInfos.IndexOf("Employment Duration") + 1] : "??";
             dateIterval = jobInfos.IndexOf("Dates Employed") != -1 ? jobInfos[jobInfos.IndexOf("Dates Employed") + 1] : "??";
+            if(empDuration.Contains("less than a year"))
+            {
+                dateIterval = dateIterval + " – " + dateIterval;
+            }
+            months.Where(mm => dateIterval.Contains(mm)).ToList().ForEach(existmm => dateIterval = dateIterval.Replace(existmm, ""));
         }
 
         public Job(string title, string dateIterval, string empDuration)
@@ -250,7 +294,10 @@ namespace Linkedin_Scrapper
 
         internal string eprint()
         {
-            var ret = "\r\n " + title + "\r\n\t" + dateIterval + "\r\n\t" + empDuration + "\r";
+            var ret = 
+                "\t" + title +
+                "\r\n\t" + dateIterval +
+                "\r\n\t" + empDuration + "\r";
             Console.WriteLine(ret);
             return ret;
         }
@@ -285,7 +332,8 @@ namespace Linkedin_Scrapper
         public string eduPrint()
         {
             var ret = "\r\n\t" + schoolName + "\r\n\t" + field + "\r\n\t" + date + "\r\n";
-            Console.WriteLine(ret);
+            Console.WriteLineFormatted("\t" + schoolName,Color.LightGoldenrodYellow);
+            Console.WriteLine("\t" + field + "\r\n\t" + date + "\r\n");
             return ret;
         }
     }
